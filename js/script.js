d797b9a7e62dc1d8977ba2294532c49df5f01f9b
notr3d <lxmrrr@gmail.com>
2017-07-21 14:49:10 +0300

    комменты в коде

--- js/script.js
+++ js/script.js
@@ -214,6 +214,7 @@ $(document).ready(function(){
       carTitle = 'Без названия';
     };
     
+    //формируем верстку
     var template = 
       '<div id="' + carId + '" class="tr">' +
         '<div class="td">' +
